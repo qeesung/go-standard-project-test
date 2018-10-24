dc0186b802f76926ba16bd8d814d41c753471169
秦世成 <qeesung@live.com>
2018-10-24 18:32:14 +0800

    Update sum.go

--- pkg/math/sum.go
+++ pkg/math/sum.go
@@ -1,5 +1,13 @@
 package mymath
 
+import (
+  "github.com/go-standard-project-test/internal/check"
+  "fmt"
+)
+
 func Sum(a, b int) int {
+  if check.CheckInt(a) {
+    fmt.Println("a is not int")
+  }
   return a+b
 }
